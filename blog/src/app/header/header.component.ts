9166a64b048b756a0678e09ba861d18c406cb4bb
Ivan Hristov <hivan1765@gmail.com>
2024-12-11 18:52:15 +0200

    Fix Navigation functionality, Add About page

--- blog/src/app/header/header.component.ts
+++ blog/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  imports: [],
+  imports: [RouterLink],
   standalone: true,
   styleUrl: './header.component.css'
 })
