ae1bf01848a14ba924702d51416d949c103dba94
Ivan Hristov <hivan1765@gmail.com>
2024-12-11 19:41:13 +0200

    Implement Firebase

---
+++ blog/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBMLUNQdrkNmQXRpy76JGwdmRmHt5QL6lY",
+        authDomain: "blog-cc0a5.firebaseapp.com",
+        databaseURL: "https://blog-cc0a5-default-rtdb.firebaseio.com",
+        projectId: "blog-cc0a5",
+        storageBucket: "blog-cc0a5.firebasestorage.app",
+        messagingSenderId: "625829152513",
+        appId: "1:625829152513:web:cd609ababe4b581852bd00",
+        measurementId: "G-PTHSS25MHH"
+    }
+};
