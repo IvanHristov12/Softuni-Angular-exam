2c1eeefd5d91e822427819b3a0cabd0af01d03e0
Ivan Hristov <hivan1765@gmail.com>
2024-12-15 17:28:44 +0200

    Add register page

--- blog/src/app/login/login.component.ts
+++ blog/src/app/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterModule],
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
