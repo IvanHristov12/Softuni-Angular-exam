3cfb5859843df617b5d1d448345aa658e6c88e3e
Ivan Hristov <hivan1765@gmail.com>
2024-12-15 18:09:39 +0200

    Add static Catalogue

--- blog/src/app/app.routes.ts
+++ blog/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { PostDetailsComponent } from './post-details/post-details.component';
+import { PostsComponent } from './posts/posts.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -12,4 +13,5 @@ export const routes: Routes = [
     { path: "login", component: LoginComponent },
     { path: "register", component: RegisterComponent },
     { path: "post/:id", component: PostDetailsComponent },
+    { path: "posts", component: PostsComponent  },
 ];
---
+++ blog/src/app/posts/posts.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-posts',
+  standalone: true,
+  imports: [],
+  templateUrl: './posts.component.html',
+  styleUrl: './posts.component.css'
+})
+export class PostsComponent {
+
+}
